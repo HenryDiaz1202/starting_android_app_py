a5118cdc9ed4bddcffc22163974c94ca0934f8ac
HenryDiaz1202 <henrydiaz1202@gmail.com>
2021-09-08 19:00:20 -0600

    probando código para navegigation, sin usar

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivymd.app import MDApp
 from kivymd.uix.button import MDRectangleFlatButton, MDRoundFlatIconButton
 from kivymd.uix.label import MDLabel, MDIcon
 from kivymd.uix.screen import MDScreen
 
 
-class MMDLabel:
-    pass
+class ActivarReconocimiento(MDApp):
+    def build(self):
+        screen = MDScreen()
+        screen.add_widget(
+            MDLabel(
+                text="Object recognition",
+                pos_hint={'x': 0, 'y': 0.2},
+                halign="center"
+            )
+        )
+        screen.add_widget(
+            Button(text="Hello")
+        )
+        return screen
 
 
 class MainApp(MDApp):
@@ -15,22 +29,22 @@ class MainApp(MDApp):
         screen.add_widget(
             MDIcon(
                 halign='center',
-                icon= "language-python",
-                pos_hint= {'x': 0, 'y': 0.3}
+                icon="language-python",
+                pos_hint={'x': 0, 'y': 0.3}
             )
         )
         screen.add_widget(
             MDLabel(
                 text="Object recognition",
-                pos_hint= {'x': 0, 'y': 0.2},
-                halign= "center"
+                pos_hint={'x': 0, 'y': 0.2},
+                halign="center"
             )
         )
         screen.add_widget(
             MDRoundFlatIconButton(
-                icon= "camera",
-                text= "Abrir cámara",
-                pos_hint={"center_x": 0.5, "center_y": 0.5}
+                icon="camera",
+                text="Abrir cámara",
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
             )
         )
         screen.add_widget(
